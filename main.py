2af9033ba9eaa529acdf47853488bc0a759111d0
EkkaKEA <canabis220@mail.ru>
2023-04-01 19:41:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # импортируем необходимые библиотеки
 import pandas as pd
 from openpyxl import Workbook
+from openpyxl.utils import get_column_letter
 from openpyxl.styles import Border, Side
 
 # чтение файла Источник.XLS и создание временной таблицы istochnik
-istochnik = pd.read_excel("Источник.XLS", usecols=[0, 1, 3, 4, 6], names=["modul", "kanal", "tipe", "signal", "koment"])
+istochnik = pd.read_excel("Источник.XLS", usecols=[0, 1, 3, 4, 6], header=None, names=["modul", "kanal", "tipe", "signal", "koment"])
 
 # создание файла Результат.xlsx
 wb = Workbook()
@@ -16,19 +17,33 @@ modul_values = istochnik["modul"].unique()
 # запись таблиц в файл Результат.xlsx
 with pd.ExcelWriter("Результат.xlsx") as writer:
     for modul in modul_values:
-        # фильтрация таблицы istochnik по значению modul
+        # фильтрация таблицы istochnik по значению modul и удаление столбца modul
         filtered_table = istochnik.loc[istochnik["modul"] == modul, ["kanal", "tipe", "signal", "koment"]]
         # запись таблицы в файл Результат.xlsx с именем листа modul
         filtered_table.to_excel(writer, sheet_name=modul, header=False, index=False)
         # получение последней записи (ячейки) в таблице и ее координат
         last_cell = writer.sheets[modul].cell(row=writer.sheets[modul].max_row, column=writer.sheets[modul].max_column)
+        # подбор ширины колонок по содержимому
+        for col in range(1, last_cell.column+1):
+            max_length = 0
+            column = get_column_letter(col)
+            for cell in writer.sheets[modul][column]:
+                try:
+                    if len(str(cell.value)) > max_length:
+                        max_length = len(str(cell.value))
+                except:
+                    pass
+            adjusted_width = (max_length + 2) * 1.2
+            writer.sheets[modul].column_dimensions[column].width = adjusted_width
         # создание стиля для обводки таблицы жирной линией
         border_style = Border(left=Side(style='thick'),
                               right=Side(style='thick'),
                               top=Side(style='thick'),
                               bottom=Side(style='thick'))
+        # получение ячеек, которые нужно обвести линией
+        cells_range = writer.sheets[modul]["A1":last_cell.coordinate]
         # применение стиля ко всем ячейкам в таблице
-        for row in writer.sheets[modul].rows:
+        for row in cells_range:
             for cell in row:
                 cell.border = border_style
 
